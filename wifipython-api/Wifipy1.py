f2cd51288af86e98cb279863952dae620ccb5b93
Taher7013 <67517051+Taher7013@users.noreply.github.com>
2021-03-22 08:01:52 +0300

    Update Wifipy1.py

--- wifipython-api/Wifipy1.py
+++ wifipython-api/Wifipy1.py
@@ -1,1 +1,40 @@
 
+# Import scapy
+import scapy.all as scapy
+# We need to create regular expressions to ensure that the input is correctly formatted.
+import re
+
+# Basic user interface header
+print(r"""  _____    __      __      ____  
+ |  __ \   \ \    / /     / __ \ 
+ | |  | | __\ \  / /_____| |  | |
+ | |  | |/ _ \ \/ /______| |  | |
+ | |__| |  __/\  /       | |__| |
+ |_____/ \___| \/         \____/ 
+                                 
+                                 """)
+print("\n****************************************************************")
+print("===============(By.DeV-O)=========≈==========================")
+print("\n================================================================")
+print("\n* Copyright of DeV-O, 2021                              *")
+print("\n* https://www.Taher7013.com                                  *")
+print("\n* https://www.youtube.com/Taher7013                          *")
+print("\n****************************************************************")
+
+# Regular Expression Pattern to recognise IPv4 addresses.
+ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+
+# Get the address range to ARP
+while True:
+    ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ")
+    if ip_add_range_pattern.search(ip_add_range_entered):
+        print(f"{ip_add_range_entered} is a valid ip address range")
+        break
+
+
+# Try ARPing the ip address range supplied by the user. 
+# The arping() method in scapy creates a pakcet with an ARP message 
+# and sends it to the broadcast mac address ff:ff:ff:ff:ff:ff.
+# If a valid ip address range was supplied the program will return 
+# the list of all results.
+arp_result = scapy.arping(ip_add_range_entered)
